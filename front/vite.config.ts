8d4b33f45b934fb7c392b3963f3d7b6fd81d428a
정태윤 <wjdxodbs52@naver.com>
2024-05-17 14:03:57 +0900

    feat: developing (#413)

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { VitePWA } from "vite-plugin-pwa";
-// import viteCompression from "vite-plugin-compression";
+import viteCompression from "vite-plugin-compression";
 // import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [
     react(),
     tsconfigPaths(),
@@ -64,10 +65,11 @@ export default defineConfig({
       },
     }),
 
-    // viteCompression({
-    //   algorithm: "gzip",
-    //   ext: ".gz",
-    // }),
+    viteCompression({
+      algorithm: "gzip",
+      ext: ".gz",
+      deleteOriginFile: true,
+    }),
   ],
   server: {
     port: 5173,
